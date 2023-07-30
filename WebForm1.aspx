0e88291d7964e51cf115c3039d4d46c8fd5ec39e
zehrasbr <zehraisbr@gmail.com>
2023-07-30 19:37:05 +0300

    ReadyMethod
    ReadyMethod

---
+++ WebForm1.aspx
@@ -0,0 +1,26 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="HazirMetot1.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="Label1" runat="server" Text="Sonuç = "></asp:Label>
+            <br />
+            <br />
+            Şifre:
+            <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+            <br />
+            Şifre Tekrar:
+            <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+            <br />
+            <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Onayla" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebForm1.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace HazirMetot1
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string[] yazilar = { "Pazartesi", "Salı", "Çarşamba" };
+            Label1.Text += String.Concat(yazilar);
+            //string yazi1, yazi2;
+            //yazi1 = "Merhaba";
+            //yazi2 = " Dünya";
+            //Label1.Text += String.Concat(yazi1, yazi2);
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            string sifre1, sifre2;
+            int sonuc;
+
+            sifre1 = TextBox1.Text;
+            sifre2 = TextBox2.Text;
+            sonuc = String.Compare(sifre1, sifre2);
+            if (sonuc == 0)
+                Label1.Text = "İki şifre aynı";
+            else if (sonuc < 0)
+                Label1.Text = "sonuc 0'dan küçük";
+            else if (sonuc > 0)
+                Label1.Text = "sonuc 0'dan büyük";
+            
+        }
+    }
+}
